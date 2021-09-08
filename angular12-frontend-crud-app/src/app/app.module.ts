b156eabc663ebcf7a539bb84416a38753e0fa38a
DIR\vincenzo.scalcione <vincenzo.scalcione@accenture.com>
2021-09-08 10:04:15 +0200

    Updating Angular Frontend

--- angular12-frontend-crud-app/src/app/app.module.ts
+++ angular12-frontend-crud-app/src/app/app.module.ts
@@ -17,6 +17,8 @@ import { MatOptionModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon'
 
 @NgModule({
   declarations: [
@@ -35,7 +37,9 @@ import { MatButtonModule } from '@angular/material/button'
     MatOptionModule,
     MatSelectModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
